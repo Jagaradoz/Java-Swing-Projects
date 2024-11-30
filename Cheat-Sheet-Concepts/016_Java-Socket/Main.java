e4629618330bd60fa50930a394af964095ebc4a3
Jar <jagaradoz@gmail.com>
2024-11-30 21:27:05 +0700

    Add A Java Socket Concept

---
+++ Cheat-Sheet-Concepts/016_Java-Socket/Main.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+// Socket is an abstraction provided by operating systems to
+// facilitate network communication and communicate via TCP
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // SERVER SOCKET
+        // used for listen for incoming connections from client
+        ServerSocket serverSocket = new ServerSocket(1234);
+
+
+        // SOCKET
+        // this two sockets are related but serves different purposes :
+        // socketFromServer         -> created by server used for communicating to client socket
+        // socketFromClient         -> created by client used for communicating to server socket
+        Socket socketFromServer = serverSocket.accept();
+        Socket socketFromClient = new Socket("localhost", 1234);
+
+
+        // BUFFERED WRITER
+        // Efficient for writing text with buffering. Requires manual flush() and newLine() for line breaks.
+        BufferedWriter writer1 = new BufferedWriter(new OutputStreamWriter(socketFromServer.getOutputStream()));
+        writer1.write("Hello World!");
+        writer1.newLine();
+        writer1.flush();
+
+        // PRINT WRITER
+        // Convenient for writing text with automatic line breaks and optional auto-flush.
+        PrintWriter writer2 = new PrintWriter(socketFromServer.getOutputStream(), true);
+        writer2.println("Hello World!");
+
+
+        // BUFFERED READER
+        // Reads text from an InputStream with buffering for improved efficiency.
+        BufferedReader reader = new BufferedReader(new InputStreamReader(socketFromServer.getInputStream()));
+        String message = reader.readLine();
+        System.out.println(message);
+
+        socketFromClient.close();
+        socketFromServer.close();
+        serverSocket.close();
+    }
+}

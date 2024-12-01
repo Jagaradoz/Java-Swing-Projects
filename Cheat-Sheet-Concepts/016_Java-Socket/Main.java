46dce12c208e49b3d9f6a39b33e0ba330e050ecc
Jar <jagaradoz@gmail.com>
2024-12-01 10:32:17 +0700

    Update A Java Socket Concept

--- Cheat-Sheet-Concepts/016_Java-Socket/Main.java
+++ Cheat-Sheet-Concepts/016_Java-Socket/Main.java
@@ -20,6 +20,8 @@ public class Main {
         Socket socketFromServer = serverSocket.accept();
         Socket socketFromClient = new Socket("localhost", 1234);
 
+        // ============================= WRITER =============================
+        // ==================================================================
 
         // BUFFERED WRITER
         // Efficient for writing text with buffering. Requires manual flush() and newLine() for line breaks.
@@ -33,12 +35,26 @@ public class Main {
         PrintWriter writer2 = new PrintWriter(socketFromServer.getOutputStream(), true);
         writer2.println("Hello World!");
 
+        // DATA OUTPUT STREAM
+        // Writes binary data (primitives, strings in UTF, etc.) in a structured format.
+        DataOutputStream writer3 = new DataOutputStream(socketFromServer.getOutputStream()); 
+        writer3.writeUTF("Hello");
+        writer3.flush();
+        
+        // ============================= READER =============================
+        // ==================================================================
 
         // BUFFERED READER
         // Reads text from an InputStream with buffering for improved efficiency.
-        BufferedReader reader = new BufferedReader(new InputStreamReader(socketFromServer.getInputStream()));
-        String message = reader.readLine();
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(socketFromServer.getInputStream()));
+        String message = reader1.readLine();
         System.out.println(message);
+
+        // DATA INPUT STREAM
+        // Reads binary data (primitives, strings in UTF, etc.) in a structured format.
+        DataInputStream reader2 = new DataInputStream(socket.getInputStream());
+        String message = reader2.readUTF();
+        System.out.println("Client : " + message);
 
         socketFromClient.close();
         socketFromServer.close();

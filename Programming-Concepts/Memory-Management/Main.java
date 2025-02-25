243ddb7035d02948af58da20d76c935fb36df3ca
Jagaradoz <jagaradoz@gmail.com>
2025-02-25 15:52:26 +0700

    Update a Memory Management concept

--- Programming-Concepts/Memory-Management/Main.java
+++ Programming-Concepts/Memory-Management/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Java Memory Management Demonstration
+        // MEMORY MANAGEMENT
 
         // 1. Method Area (Stores static and class-level data.)
         // - Stores class-level data such as method code, static variables, and runtime constant pool.
